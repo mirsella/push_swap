330fd85017c5e2511ab8a58d6f67930886170e70
mirsella <mirsella@protonmail.com>
2023-01-04 11:40:53 +0100

    best solution for now, ready

--- Makefile
+++ Makefile
@@ -1,9 +1,10 @@
 SHARED_SRCS = $(addprefix shared_srcs/, pile.c pile_action_basic.c pile_action_rotate.c parser.c)
+SHARED_OBJS = $(SHARED_SRCS:.c=.o)
 
-SRCS = $(addprefix push_swap_srcs/, push_swap.c sort.c sort_algo.c sort_algo_advanced.c) $(SHARED_SRCS)
+SRCS = $(addprefix push_swap_srcs/, push_swap.c sort.c sort_algo.c sort_algo_advanced.c)
 OBJS = $(SRCS:.c=.o)
 
-BSRCS = $(addprefix checker_srcs/, checker.c) $(SHARED_SRCS)
+BSRCS = $(addprefix checker_srcs/, checker.c)
 BOBJS = $(BSRCS:.c=.o)
 
 CC = cc
@@ -15,20 +16,20 @@ NAME = push_swap
 
 all: $(NAME)
 
-$(NAME): $(LIBFT) $(OBJS)
-	$(CC) $(CFLAGS) -o $(NAME) $(OBJS) $(LIBFT)
+$(NAME): $(LIBFT) $(OBJS) $(SHARED_OBJS)
+	$(CC) $(CFLAGS) -o $(NAME) $(OBJS) $(SHARED_OBJS) $(LIBFT)
 
 clean:
 	make -C libft clean
-	rm $(OBJS) $(BOBJS)
+	rm $(OBJS) $(BOBJS) $(SHARED_OBJS)
 
 fclean: clean
 	rm $(NAME) $(BNAME) $(LIBFT)
 
 re: fclean all
 
-bonus: $(LIBFT) $(BOBJS)
-	$(CC) $(CFLAGS) -o $(BNAME) $(BOBJS) $(LIBFT)
+bonus: $(LIBFT) $(BOBJS) $(SHARED_OBJS)
+	$(CC) $(CFLAGS) -o $(BNAME) $(BOBJS) $(SHARED_OBJS) $(LIBFT)
 
 $(LIBFT):
 	make -C libft
--- push_swap_srcs/sort_algo_advanced.c
+++ push_swap_srcs/sort_algo_advanced.c
@@ -6,7 +6,7 @@
 /*   By: mirsella <mirsella@protonmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/03 22:16:28 by mirsella          #+#    #+#             */
-/*   Updated: 2023/01/03 22:26:53 by mirsella         ###   ########.fr       */
+/*   Updated: 2023/01/04 11:34:39 by mirsella         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,30 +22,32 @@ void	sort_pb_to_pa(t_piles *a, t_piles *b)
 	}
 }
 
-void	should_rr_or_r(t_piles *a, t_piles *b, int *should_rr)
+size_t	should_rr_or_r(t_piles *a, t_piles *b, int should_rr)
 {
-	if (*should_rr && b->size > 0)
+	if (b->size > 0 && should_rr)
 	{
-		(*should_rr) = 0;
 		rotate_pp(a, b);
 		ft_putstr("rr\n");
+		return (1);
 	}
-	else
-	{
-		rotate_p(a);
-		ft_putstr("ra\n");
-	}
+	rotate_p(a);
+	ft_putstr("ra\n");
+	return (0);
 }
 
-void	goto_num_rr(t_piles *a, t_piles *b, int num, size_t i)
+void	goto_num_rr(t_piles *a, t_piles *b, size_t i, size_t counter)
 {
-	static int	should_rr = 0;
+	static size_t	rr_count = 0;
+	int				num;
 
+	if (counter == 0)
+		rr_count = 0;
+	num = a->p[i];
 	if (i <= a->size / 2)
 	{
 		while (a->p[0] != num)
 		{
-			should_rr_or_r(a, b, &should_rr);
+			rr_count += should_rr_or_r(a, b, counter / 2 > rr_count);
 		}
 	}
 	else
@@ -56,14 +58,12 @@ void	goto_num_rr(t_piles *a, t_piles *b, int num, size_t i)
 			ft_putstr("rra\n");
 		}
 	}
-	should_rr = !should_rr;
 }
 
-int	goto_closest_below_pa(t_piles *a, t_piles *b, int max)
+int	goto_closest_below_pa1(t_piles *a, t_piles *b, int max, size_t counter)
 {
 	size_t	start;
 	size_t	end;
-	int		num;
 
 	start = 0;
 	end = 0;
@@ -73,25 +73,53 @@ int	goto_closest_below_pa(t_piles *a, t_piles *b, int max)
 		end++;
 	if (start == end && start == a->size)
 		return (0);
-	num = a->p[ft_llmin(start, a->size - 1 - end)];
-	goto_num_rr(a, b, num, ft_llmin(start, a->size - 1 - end));
+	goto_num_rr(a, b, ft_llmin(start, a->size - 1 - end), counter);
 	return (1);
 }
+
+// int	goto_closest_below_pa2(t_piles *a, t_piles *b, int max, size_t counter)
+// {
+// 	size_t	start;
+// 	size_t	end;
+// 	int		index;
+// 
+// 	start = 0;
+// 	end = a->size - 1;
+// 	while (a->p[start] > max && start < a->size)
+// 		start++;
+// 	while (a->p[end] > max && end > 0)
+// 		end--;
+// 	if (start == a->size)
+// 		return (0);
+// 	if (start < a->size - 1 - end)
+// 		index = start;
+// 	else
+// 		index = end;
+// 	goto_num_rr(a, b, index, counter);
+// 	return (1);
+// }
 
 void	sort_advanced(t_piles *a, t_piles *b, int chunk)
 {
 	int		limit;
 	int		chunkn;
+	size_t	counter;
 
 	chunkn = 1;
 	get_limit(a, b, chunk);
 	while (chunkn < chunk)
 	{
 		limit = get_limit(a, b, chunkn);
-		while (goto_closest_below_pa(a, b, limit))
+		counter = 0;
+		while (goto_closest_below_pa1(a, b, limit, counter++))
 		{
 			push_p(b, a);
 			ft_putstr("pb\n");
+			if (b->size > 1 && a->size > 1 && b->p[0] < b->p[1])
+			{
+				swap_pp(a, b);
+				ft_putstr("ss\n");
+			}
 		}
 		chunkn++;
 	}
